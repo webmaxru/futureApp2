fdcb88b7b5186bef8107d85b195a909cf217ae02
Maxim Salnikov <salnikov@gmail.com>
2016-02-24 16:28:20 +0100

    js file removed

--- app/pages/angular-fire/angular-fire.ts
+++ app/pages/angular-fire/angular-fire.ts
@@ -3,26 +3,16 @@ import {Observable} from 'rxjs/Observable';
 
 import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
 
-/*
-  Generated class for the AngularFirePage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Page({
   templateUrl: 'build/pages/angular-fire/angular-fire.html',
-
   providers: [FIREBASE_PROVIDERS, defaultFirebase('https://futureapp2.firebaseio.com')]
 })
 export class AngularFirePage {
 
   questions: Observable<any>
 
-  constructor(af: AngularFire) {
-    // Get an observable of a synchronized array from <firebase-root>/questions
+  constructor(af: AngularFire, private nav: NavController) {
     this.questions = af.list('/posts');
-
-
   }
 
 }
